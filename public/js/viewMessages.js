c98ef84167d0d993249d15852745537eb3bca6e6
gtang1626 <gtang1626@gmail.com>
2021-07-22 17:37:36 +0000

    log number of failed attempts

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,3 +1,5 @@
+let failedAttempts = 0;
+
 const getMessages = () => {
     const passcode = document.querySelector("#passcode");
     
@@ -20,7 +22,17 @@ const getMessages = () => {
             console.log("match not found")
             const message = document.querySelector("#message")
             message.style.color = "#ff0000"
-            message.innerHTML = "Message not found"          
+            message.innerHTML = "Message not found"  
+            failedAttempts ++;
+            console.log(failedAttempts);  
+           
+            /*
+            if (failedAttempts > 3){
+                console.log("Wait");
+                let waitMinutes = failedAttempts - 3;
+                
+            } 
+            */
         }
 
         passcode.value = "";
