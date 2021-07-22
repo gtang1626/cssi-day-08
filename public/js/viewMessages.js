6054ebe6774b7d0ac3059aa6bb92ade075d3d13a
gtang1626 <gtang1626@gmail.com>
2021-07-22 17:29:33 +0000

    Added error message if password is not found

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -4,12 +4,25 @@ const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val()
+        let found = false;
+        
         for (let key in data) {
             if (key === passcode.value){
                 console.log("match found")
                 const message = document.querySelector("#message")
+                message.style.color = "#000000"
                 message.innerHTML = data[key]
-            }            
+                found = true
+            }
         }
+
+        if (found === false){
+            console.log("match not found")
+            const message = document.querySelector("#message")
+            message.style.color = "#ff0000"
+            message.innerHTML = "Message not found"          
+        }
+
+        passcode.value = "";
     })
 }
